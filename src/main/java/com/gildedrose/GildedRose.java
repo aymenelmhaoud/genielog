fab306215cb430417d7703ab1f74f9c548247fff
unknown <aymen.elmhaoud6@gmail.com>
2022-10-07 16:52:04 +0100

    third commit

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,47 +8,8 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            switch(items[i].name){
-                case "Aged Brie":
-                items[i].sellIn-=1;
-                if(items[i].quality<50){
-                    items[i].quality++;
-                    if(items[i].quality<50 && items[i].sellIn<0){
-                        items[i].quality++;
-                    }
-                  
-                    
-                }
-                break;
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    if (items[i].quality<50){
-                        items[i].quality++;
-                        if(items[i].quality<50 && items[i].sellIn<11){
-                            items[i].quality++;
-                        }
-                        if(items[i].quality<50 && items[i].sellIn<6){
-                            items[i].quality++;
-                        }
-                        items[i].sellIn--;
-                        if( items[i].sellIn<0){
-                            items[i].quality=0;
-                        } 
-                    }
-                    break;
-                default :
-                      if(!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
-                        items[i].sellIn--;
-                        if(items[i].quality>0){
-                            if(items[i].sellIn<0){
-                                items[i].quality-=2;
-                            }else{
-                                items[i].quality-=1;
-                            }
-                        }
-                      }  
-                        
-            }
+        for (Item item : items) {
+            item.updateQuality();
 
          }
          
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -23,8 +23,7 @@ class GildedRoseTest {
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertThat(element.name, is("foo"));
-    assertThat(element.name, is(not(("Backstage passes to a TAFKAL80ETC concert"))));
-    assertThat(element.name, is(not(("Aged Brie"))));
+   
 
   }
 
@@ -35,12 +34,21 @@ class GildedRoseTest {
   @DisplayName("Test that the quality will decrement")
   void testdecrementation() {
     Item[] items = new Item[] {new Item("boo", 2, 0),
-    new Item("foo", 2, 6)};
+    new Item("foo", 2, 6),
+    new Item(Item.CAKE, -1, 49),
+    new Item(Item.CAKE, 0, 49),
+    new Item(Item.CAKE, 1, 49),
+    new Item("boo", -1, 0)
+  };
     GildedRose app = new GildedRose(items);
     app.updateQuality();
   
     assertThat(app.items[0].quality, is(0));
     assertThat(app.items[1].quality, is(5));
+    assertThat(app.items[2].quality, is(45));
+    assertThat(app.items[3].quality, is(45));
+    assertThat(app.items[4].quality, is(47));
+    assertThat(app.items[5].quality, is(0));
   
 
     
@@ -54,13 +62,23 @@ class GildedRoseTest {
  * 
  */
 @Test
-@DisplayName("Test that Aged Brie quality increases as time goes by")
-void aged_brie_quality_increases_in_time() {
-  Item[] items = new Item[] {new Item("Aged Brie", 2, 0),
-   new Item("Aged Brie", 2, 50),
-   new Item("Backstage passes to a TAFKAL80ETC concert", 4, 44),
-   new Item("Sulfuras, Hand of Ragnaros", 0, 50),
-   new Item("Backstage passes to a TAFKAL80ETC concert", 6, 44)};
+@DisplayName("Test  quality increases as time goes by")
+void quality_increases_in_time() {
+  Item[] items = new Item[] {new Item(Item.AGED_BRIE, 2, 0),
+   new Item(Item.AGED_BRIE, 2, 50),
+   new Item(Item.BACKSTAGE, 4, 44),
+   new Item(Item.SULFURAS, 0, 50),
+   new Item(Item.AGED_BRIE, 0, 2),
+   new Item(Item.BACKSTAGE, 11, 17),
+    new Item(Item.BACKSTAGE, 0, 49),
+    new Item(Item.SULFURAS, 0, 40),
+   
+     new Item(Item.AGED_BRIE, 1, 40),
+    
+    
+     new Item(Item.SULFURAS, -1, 49),
+    
+  };
     GildedRose app = new GildedRose(items);
     app.updateQuality();
   
@@ -68,59 +86,17 @@ void aged_brie_quality_increases_in_time() {
     assertThat(app.items[1].quality, is(50));
     assertThat(app.items[2].quality, is(47));
     assertThat(app.items[3].quality, is(50));
-    assertThat(app.items[4].quality, is(46));
-}
-@Test
-@DisplayName("Test that if quality less than 50 the quality increase")
-void quality_increase() {
-  Item[] items = new Item[] {new Item("Aged Brie", 2, 0),
-   new Item("Aged Brie", 2, 40),
-   new Item("Backstage passes to a TAFKAL80ETC concert", 8, 49),
-   new Item("Sulfuras, Hand of Ragnaros", 0, 40),
-   new Item("Backstage passes to a TAFKAL80ETC concert", 11, 17),
-   new Item("Backstage passes to a TAFKAL80ETC concert", 4, 48)
-  };
-    GildedRose app = new GildedRose(items);
-    app.updateQuality();
-  
-    assertThat(app.items[0].quality, is(1));
-    assertThat(app.items[1].quality, is(41));
-    assertThat(app.items[2].quality, is(50));
-    assertThat(app.items[3].quality, is(40));
-    assertThat(app.items[4].quality, is(18));
-    assertThat(app.items[5].quality, is(50));
-}
-   
-@Test
-  @DisplayName("Test that the quality will decrement")
-  void testdecrementation2() {
-    Item[] items = new Item[] {new Item("Aged Brie", 0, 2),
-    new Item("Aged Brie", 0, 40),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49),
-    new Item("Sulfuras, Hand of Ragnaros", 0, 40),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 0, 17),
-     new Item("boo", 0, 80),
-     new Item("Aged Brie", 1, 40),
-     new Item("boo", -1, 0),
-     new Item("Sulfuras, Hand of Ragnaros", -1, 80), 
-     new Item("Sulfuras, Hand of Ragnaros", -1, 49),
-     new Item("Aged Brie", -1, 49)};
-     GildedRose app = new GildedRose(items);
-    app.updateQuality();
-    assertThat(app.items[0].quality, is(4));
-    assertThat(app.items[1].quality, is(42));
-    assertThat(app.items[2].quality, is(0));
-    assertThat(app.items[3].quality, is(40));
-    assertThat(app.items[4].quality, is(0));
-    assertThat(app.items[5].quality, is(78));
-    assertThat(app.items[5].sellIn, is(-1));
-    assertThat(app.items[6].quality, is(41));
-    assertThat(app.items[7].quality, is(0));
-    assertThat(app.items[8].quality, is(80));
-    
+    assertThat(app.items[4].quality, is(4));
+    assertThat(app.items[5].quality, is(18));
+    assertThat(app.items[6].quality, is(0));
+    assertThat(app.items[7].quality, is(40));
+    assertThat(app.items[8].quality, is(41));
     assertThat(app.items[9].quality, is(49));
-    assertThat(app.items[10].quality, is(50));
+    
 }
+
+   
+
 @Test
   @DisplayName("Test tostring")
   void testtostring() 
